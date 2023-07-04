7a1e1ab5c01b5a942c94656ace9c7a9c2c832484
joules2w <joules2wat@protonmail.com>
2023-07-04 15:54:16 +0530

    Update package-lock.json

--- App.js
+++ App.js
@@ -41,13 +41,3 @@ const App = () => {
 export default App;
 
 
-// import { View, Text } from 'react-native'
-// import React from 'react'
-
-// export default function App() {
-//   return (
-//     <View>
-//       <Text>App</Text>
-//     </View>
-//   )
-// }
